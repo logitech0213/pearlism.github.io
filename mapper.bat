45eb603498554beced0de8c5f3aa50bd4522ef91
Landen.419 <167775790+Pearlism@users.noreply.github.com>
2024-10-30 19:44:50 -0400

    Update mapper.bat

--- mapper.bat
+++ mapper.bat
@@ -2,8 +2,7 @@
 set "bazarPath=C:\Program Files\Snippingtool\bazar.sys"
 set "mapperPath=C:\Program Files\Snippingtool\map.exe"
 
-:: Drag bazar.sys onto mapper.exe
-echo Running mapper.exe with bazar.sys
-"%mapperPath%" "%bazarPath%"
+echo Running mapper.exe with bazar.sys > log.txt 2>&1
+"%mapperPath%" "%bazarPath%" >> log.txt 2>&1
 
 pause
