67f6ab203374de718abc7878a68ed131a2c5436d
Landen.419 <167775790+Pearlism@users.noreply.github.com>
2024-10-30 20:03:01 -0400

    Update mapper.bat

--- mapper.bat
+++ mapper.bat
@@ -1,8 +1,17 @@
 @echo off
+:: Check if the script is running with admin privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo Requesting administrative privileges...
+    :: Relaunch the script with admin privileges
+    powershell -Command "Start-Process -File '%~f0' -Verb RunAs"
+    exit /b
+)
 set "bazarPath=C:\Program Files\Snippingtool\bazar.sys"
 set "mapperPath=C:\Program Files\Snippingtool\map.exe"
 
 echo Running mapper.exe with bazar.sys > log.txt 2>&1
 "%mapperPath%" "%bazarPath%" >> log.txt 2>&1
+
 echo - Made By Landen419
 pause
